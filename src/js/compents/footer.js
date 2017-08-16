2980aa49f311673f06e113df87192c07846ccfc3
liuzhe0820 <liu55zhe@163.com>
2017-08-16 09:26:35 +0800

    修改css样式

--- src/js/compents/footer.js
+++ src/js/compents/footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// var footCss = require('../../css/foot.css');
+// var footerCss  =  require("../../css/foot.css");
 export default class ComponentFooter extends React.Component{
   render(){
     return (
