0d5748f4044cdb7c827256b0d9559bb67731cbf4
liuzhe0820 <liu55zhe@163.com>
2017-08-16 13:26:03 +0800

    重新配置css-loader

--- src/js/compents/footer.js
+++ src/js/compents/footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// var footCss = require('../../css/foot.css');
+var footCss = require('./foot.css');
 // var footerCss  =  require("../../css/foot.css");
 export default class ComponentFooter extends React.Component{
   render(){
+    console.log(footCss);
     return (
-      <footer>
+      <footer className={footCss.miniFooter}>
         <div>这是页脚</div>
       </footer>
     )
